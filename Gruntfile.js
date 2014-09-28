e59dbda0732b8c13fbca033963982f690f65bc19
Kiril Vatev <vatev.1@gmail.com>
2014-09-28 15:04:49 -0400

    adding grunt

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+/* jshint node: true */
+var fs = require('fs');
+
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'toast.js',
+                dest: 'toast.min.js'
+            }
+        }
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Default task(s).
+    grunt.registerTask('default', ['uglify']);
+
+};
